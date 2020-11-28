a14b64a7d4588f2f880c761486166e84531ebde8
Ampernic <44705058+Ampernic@users.noreply.github.com>
2020-11-28 17:40:10 +0300

    Обновление 1_0.6.5
    ------------ Клиент -----------
    [+] Добавлена адекватная обработка кика при конфликте версий (409)
    [+] Возвращены пупап-уведомления при новых сообщениях
    [~] Незначительные правки дизайна
                 - Добавлено лого
                 - Чуть изменены цвета
    
    ------------ Сервер -----------
    [+] Добавлено логирование выхода пользователей
    [+] Добавлено логирование кика
    [~] Незначительные правки дизайна
                 - Добавлено лого
                 - Чуть изменены цвета
                 - Добавлены разделители

--- chat_client.py
+++ chat_client.py
@@ -1,3 +1,4 @@
+# Imports
 import sys
 import socket
 import select
@@ -6,42 +7,76 @@ from colorama import init, Fore, Back, Style
 import socket
 import threading
 import time
+import os
+
 from win10toast import ToastNotifier
-import getpass
 
+# Inits
 toaster = ToastNotifier()
-
 init(convert=True)
 
-print("\033[36m" + """
+# Logo
+print(Fore.CYAN +"""
+                                _
+                             ,:'/   _..._
+                            // ( `""-.._.'
+                            \| /    6\___
+                            |     6      4
+                            |            /
+                            \_       .--'
+                            (_'---'`)
+                            / `'---`()
+                          ,'        |
+         ,            .'`          |
+         )\       _.-'             ;
+        / |    .'`   _            /
+       /` /   .'       '.        , |
+      /  /   /           \   ;   | |
+      |  \  |            |  .|   | |
+       \  `"|           /.-' |   | |
+        '-..-\       _.;.._  |   |.;-.
+              \    <`.._  )) |  .;-. ))
+              (__.  `  ))-'  \_    ))'
+                  `'--"`       `--"
+                  
+    ███╗   ███╗ █████╗ ████████╗██╗  ██╗
+    ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
+    ██╔████╔██║███████║   ██║   ███████║
+    ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
+    ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
+    ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝  Ampernic`s fork (Psinamper client)""" + Fore.WHITE)
+
+time.sleep(2)
+os.system('cls||clear')
+
+# Info
+print(Fore.CYAN + """
     ███╗   ███╗ █████╗ ████████╗██╗  ██╗
     ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
     ██╔████╔██║███████║   ██║   ███████║
     ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
     ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
     ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
-
-    -----------------------------------
-    Version: Alpha 1_0.6.2
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    Version: Alpha 1_0.6.5
     Author: Wiskey666
-    Forked: Ampernic
-    Mail: ampernic@inbox.ru
-    DS: yourmomgay#1488
-    -----------------------------------
+    Forked by: Ampernic
+    Mail: ampernic@list.ru
+    DS: ampernic#9787""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
 
     This is a console chat for special lovers of this shit.
     The chat will be improved (both the client side and the server side).
     Thank you for noticing and trying it!
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    """+ Fore.WHITE)
 
-    -----------------------------------
-        IP     Port        Name
-    aoa.pp.ua  55555  #general_server
-    -----------------------------------
-""")
-print(Style.RESET_ALL)
-
+# Client Settings
 IP = input('    IP: ')
 PORT = int(input('    Port: '))
+ver = "Alpha 1_0.6.5"
 
 # Choosing Nickname
 nickname = input('    Username: ')
@@ -58,17 +93,26 @@ def receive():
     while True:
         try:
             # Receive Message From Server
-            # If 'NICK' Send Nickname
             message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
-            else:
+            
+            #Version Check
+            #If Server Sends Conflict Code - Kick
+            if "Code 409" in message:
                 print(Fore.GREEN +'░' + Fore.YELLOW + '░' + Fore.GREEN + '░ ' + Fore.WHITE + message)
-                if "<"+ nickname +">" in message or "[+]" in message or "[-]" in message:
-                    continue
+                client.close()
+                break
+            else:
+                # If 'NICK' Send Nickname
+                if message == 'NICK':
+                    client.send(nickname.encode('utf-8'))
+                    client.send(ver.encode('utf-8'))
                 else:
-                   toaster.show_toast("MATH", message, threaded=True)
-                #print(Style.RESET_ALL)
+                    print(Fore.GREEN +'░' + Fore.YELLOW + '░' + Fore.GREEN + '░ ' + Fore.WHITE + message)
+                    #Check For New Messages
+                    if "<"+ nickname +">" in message or "[+]" in message or "[-]" in message:
+                        continue
+                    else:
+                       toaster.show_toast("MATH", message, threaded=True)
         except:
             # Close Connection When Error 
             print(Fore.GREEN +'>' + Fore.YELLOW + '>' + Fore.GREEN + '> ' + Fore.RED + "[-] An error occured!")
@@ -79,8 +123,13 @@ def receive():
 def write():
     while True:
         try:
-            message = '<{}> {}'.format(nickname, input(''))
-            client.send(message.encode('utf-8'))
+            message_text = input('')
+            # Check for blank massage
+            if message_text == '':
+                continue
+            else:
+                message = '<{}> {}'.format(nickname, message_text)
+                client.send(message.encode('utf-8'))
         except:
             pass
 
@@ -90,3 +139,4 @@ receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
--- chat_server.py
+++ chat_server.py
@@ -4,36 +4,75 @@ import errno
 from colorama import init, Fore, Back, Style
 import socket
 import threading
+import os
+import time
 
 init(convert=True)
 
-print("\033[36m" + """
+print(Fore.CYAN +"""
+                                _
+                             ,:'/   _..._
+                            // ( `""-.._.'
+                            \| /    6\___
+                            |     6      4
+                            |            /
+                            \_       .--'
+                            (_'---'`)
+                            / `'---`()
+                          ,'        |
+         ,            .'`          |
+         )\       _.-'             ;
+        / |    .'`   _            /
+       /` /   .'       '.        , |
+      /  /   /           \   ;   | |
+      |  \  |            |  .|   | |
+       \  `"|           /.-' |   | |
+        '-..-\       _.;.._  |   |.;-.
+              \    <`.._  )) |  .;-. ))
+              (__.  `  ))-'  \_    ))'
+                  `'--"`       `--"
+                  
+    ███╗   ███╗ █████╗ ████████╗██╗  ██╗
+    ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
+    ██╔████╔██║███████║   ██║   ███████║
+    ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
+    ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
+    ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝  Ampernic`s fork (Psinamper server core)""" + Fore.WHITE)
+
+time.sleep(2)
+os.system('cls||clear')
+
+print(Fore.CYAN + """
     ███╗   ███╗ █████╗ ████████╗██╗  ██╗
     ████╗ ████║██╔══██╗╚══██╔══╝██║  ██║
     ██╔████╔██║███████║   ██║   ███████║
     ██║╚██╔╝██║██╔══██║   ██║   ██╔══██║
     ██║ ╚═╝ ██║██║  ██║   ██║   ██║  ██║
     ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
-
-    -----------------------------------
-    Version: Alpha 1_0.6.2
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    Version: Alpha 1_0.6.5
     Author: Wiskey666
-    Forked: Ampernic
-    Mail: 1488step@horsefucker.org
-    DS: yourmomgay#1488
-    -----------------------------------
+    Forked by: Ampernic
+    Mail: ampernic@list.ru
+    DS: ampernic#9787""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
 
     This is a console chat for special lovers of this shit.
     The chat will be improved (both the client side and the server side).
     Thank you for noticing and trying it!
-
-    -----------------------------------
-""")
-print(Style.RESET_ALL)
+""" + "\33[90m" + """
+    -----------------------------------""" + Fore.CYAN + """
+    """+ Fore.WHITE)
 
 # Connection Data
-host = '127.0.0.1'
-port = 55555
+host= input('    Set host IP: ')
+port= int(input('    Set host port: '))
+channel_name = "#" + input('    Set channel name: ')
+ver= 'Alpha 1_0.6.5'
+
+print("\33[90m" + """
+    -----------------------------------"""+ Fore.WHITE)
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,6 +102,8 @@ def handle(client):
             client.close()
             nickname = nicknames[index]
             broadcast('[-] {} left the server!'.format(nickname).encode('utf-8'))
+            print(Fore.YELLOW + "[-] {} left the server!".format(nickname))
+            print("\33[90m" + """    -----------------------------------"""+ Fore.WHITE)
             nicknames.remove(nickname)
             break
 
@@ -71,21 +112,44 @@ def receive():
     while True:
         # Accept Connection
         client, address = server.accept()
-        print("[+] Connected with {}".format(str(address)))
+        print(Fore.YELLOW +"[+] Connected with {}".format(str(address)) + Fore.WHITE)
 
         # Request And Store Nickname
         client.send('NICK'.encode('utf-8'))
         nickname = client.recv(1024).decode('utf-8')
+        client_ver = client.recv(1024).decode('utf-8')
         nicknames.append(nickname)
         clients.append(client)
+       
 
         # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("[+] {} joined!".format(nickname).encode('utf-8'))
-        client.send('[+] Connected to #general_server!'.encode('utf-8'))
+        print("    Nickname is {}".format(nickname))
+        # Checking Version
+        if client_ver == ver:
+            print("    Client version: " + client_ver)
+            print("\33[90m" + """    -----------------------------------"""+ Fore.WHITE)
+            broadcast("[+] {} joined!".format(nickname).encode('utf-8'))
+            client.send("[+] Connected to {}!".format(channel_name).encode('utf-8'))
+        else:
+            print("    Client version: " + client_ver + Fore.RED + " (TOO OLD!)" + Fore.WHITE)
+            print("\33[90m" + """    -----------------------------------"""+ Fore.WHITE)
+            client.send("[-] You have been kicked from the server! Code 409".encode('utf-8'))
+            
+        # Check Version Of Client
+        if client_ver == ver:
+            # If All Is Good - Start Handling Thread For Client
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+        else:
+            # If Old - Kick Him
+            broadcast('[-] {} kicked from the server! Too old version.'.format(nickname).encode('utf-8'))
+            print(Fore.YELLOW + "[-] {} kicked from the server! Too old version.".format(nickname) + Fore.WHITE)
+            clients.remove(client)
+            client.close()
+            nicknames.remove(nickname)
+            print("\33[90m" + """    -----------------------------------"""+ Fore.WHITE)
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
+#Start Listening
 receive()
+
+
